c7fda7cb3a700109fdee1fddc311cc6f23ea6937
Junne <junne.bai@gmail.com>
2016-12-28 17:26:52 +0800

    add some code

--- app/src/main/java/com/github/junne/androidweather/modules/main/adapter/WeatherAdapter.java
+++ app/src/main/java/com/github/junne/androidweather/modules/main/adapter/WeatherAdapter.java
@@ -3,7 +3,10 @@ package com.github.junne.androidweather.modules.main.adapter;
 import android.content.Context;
 import android.support.v7.widget.ContentFrameLayout;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
+import com.github.junne.androidweather.R;
 import com.github.junne.androidweather.component.AnimRecyclerViewAdapter;
 import com.github.junne.androidweather.modules.main.domain.Weather;
 
@@ -23,6 +26,46 @@ public class WeatherAdapter extends AnimRecyclerViewAdapter<RecyclerView.ViewHol
 
     private Weather mWeatherData;
 
-    
+    public WeatherAdapter(Weather weatherData) {
+        this.mWeatherData = weatherData;
+    }
 
+    @Override
+    public int getItemViewType(int position) {
+
+        if (position == WeatherAdapter.TYPE_ONE) {
+            return WeatherAdapter.TYPE_ONE;
+        }
+
+        if (position == WeatherAdapter.TYPE_TWO) {
+            return WeatherAdapter.TYPE_TWO;
+        }
+
+        if (position == WeatherAdapter.TYPE_THREE) {
+            return WeatherAdapter.TYPE_THREE;
+        }
+
+        if (position == WeatherAdapter.TYPE_FOUR) {
+            return WeatherAdapter.TYPE_FOUR;
+        }
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        mContext = parent.getContext();
+        switch (viewType) {
+            case TYPE_ONE:
+                return new NowWeatherViewHolder(
+                        LayoutInflater.from(mContext).inflate(R.layout.item_temperature, parent, false));
+        }
+
+        return super.onCreateViewHolder(parent, viewType);
+    }
+
+    class NowWeatherViewHolder extends RecyclerView.ViewHolder {
+
+
+    }
 }
